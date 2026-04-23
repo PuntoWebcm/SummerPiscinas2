c3fb12bdcea0f9124d2ffa42545dc1aee4b82784
Eliana <e.tallone@itecriocuarto.org.ar>
2026-04-23 20:27:31 -0300

    Habilitar imágenes en producción

--- configuracion/urls.py
+++ configuracion/urls.py
@@ -5,9 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tienda.urls')),  # <-- Esto conecta las rutas de tu app
+    path('', include('tienda.urls')),
 ] 
 
-# Esto es para que Django pueda servir las fotos de las piletas en desarrollo
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Sacamos el IF para que funcione siempre, incluso en el servidor real
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
