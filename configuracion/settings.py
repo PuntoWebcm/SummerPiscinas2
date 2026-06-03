58fb6b1be2faa6d4ad2feeaecd8d02f7eb9a8fa7
Eliana <e.tallone@itecriocuarto.org.ar>
2026-06-03 14:41:06 -0300

    restaurando conexion a postgres

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'configuracion.wsgi.application'
 
 # BASE DE DATOS LOCAL INTERNA - SOLUCIÓN INMEDIATA
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
