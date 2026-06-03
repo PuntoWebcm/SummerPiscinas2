1997089207449f58c6706c029c899b21012f54ca
Eliana <e.tallone@itecriocuarto.org.ar>
2026-06-03 16:54:32 -0300

    Tratando de solucionar

--- configuracion/settings.py
+++ configuracion/settings.py
@@ -84,12 +84,11 @@ WSGI_APPLICATION = 'configuracion.wsgi.application'
 # BASE DE DATOS LOCAL INTERNA - SOLUCIÓN INMEDIATA
 # CONEXIÓN DIRECTA Y SEGURA A POSTGRESQL (PAGO)
 DATABASES = {
-'default': dj_database_url.config(
-default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-conn_max_age=600
-)
-
-} 
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        conn_max_age=600
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
